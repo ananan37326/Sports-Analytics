1dc98e345470c117633bf5fdf7077469b501dfa1
Ananan <1705053@ugrad.cse.buet.ac.bd>
2020-08-14 11:42:51 +0600

    Code with exercise 3 done

--- 1LoadInData.py
+++ 1LoadInData.py
@@ -8,7 +8,8 @@ with open('Statsbomb/data/competitions.json') as f:
     competitions = json.load(f)
     
 #Womens World Cup 2019 has competition ID 72
-competition_id=72
+#Mens World Cup 2018 has competition ID 43
+#competition_id=43
 
 #Womens World Cup 2019 has competition ID 72
 competition_id=72
@@ -24,6 +25,10 @@ matches[0]['home_team']
 matches[0]['home_team']['home_team_name']
 matches[0]['away_team']['away_team_name']
 
+print(f"Competition : {matches[0]['competition']['competition_name']}")
+
+team_required ="Sweden Women's"
+
 #Print all match results
 for match in matches:
     home_team_name=match['home_team']['home_team_name']
@@ -32,18 +37,20 @@ for match in matches:
     away_score=match['away_score']
     describe_text = 'The match between ' + home_team_name + ' and ' + away_team_name
     result_text = ' finished ' + str(home_score) +  ' : ' + str(away_score)
-    print(describe_text + result_text)
+    if home_team_name==team_required or away_team_name==team_required:
+        print(describe_text + result_text)
 
 #Now lets find a match we are interested in
-home_team_required ="England"
-away_team_required ="Sweden"
+home_team_required ="England Women's"
+away_team_required ="Sweden Women's"
 
 #Find ID for the match
 for match in matches:
     home_team_name=match['home_team']['home_team_name']
     away_team_name=match['away_team']['away_team_name']
-    if (home_team_name==home_team_required) and (away_team_name==away_team_required):
+    if (home_team_name==home_team_required) and (away_team_name==away_team_required) :
         match_id_required = match['match_id']
+
 print(home_team_required + ' vs ' + away_team_required + ' has id:' + str(match_id_required))
         
 #Exercise: 
